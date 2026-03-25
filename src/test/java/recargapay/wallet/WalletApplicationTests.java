4660cd19edb62557e8cae7191ccb5636aeaf9d2c
João André Simiquelli <andresimiquelli@hotmail.com>
2026-03-25 20:35:54 -0300

    test: configure test container

--- src/test/java/recargapay/wallet/WalletApplicationTests.java
+++ src/test/java/recargapay/wallet/WalletApplicationTests.java
@@ -3,15 +3,8 @@ package recargapay.wallet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-		"spring.datasource.url=jdbc:h2:mem:wallet;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=",
-		"spring.jpa.hibernate.ddl-auto=create-drop",
-		"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
-})
-class WalletApplicationTests {
+@SpringBootTest
+class WalletApplicationTests extends PostgresIntegrationTest {
 
 	@Test
 	void contextLoads() {
