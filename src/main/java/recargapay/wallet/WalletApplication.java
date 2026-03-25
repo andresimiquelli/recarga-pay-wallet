6ec6873846e8795978f86cac7e546d3936246420
João André Simiquelli <andresimiquelli@hotmail.com>
2026-03-24 22:31:42 -0300

    first commit

---
+++ src/main/java/recargapay/wallet/WalletApplication.java
@@ -0,0 +1,20 @@
+package recargapay.wallet;
+
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+}
